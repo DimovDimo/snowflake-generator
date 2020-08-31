0c88fb2dcb0c9ec309ee85f77ec236ec354578f2
DimovDimo <d.dimov1996@gmail.com>
2020-08-31 14:06:57 +0300

    added function getIterations

--- app/resources/js/snowflake-generator.js
+++ app/resources/js/snowflake-generator.js
@@ -1,10 +1,16 @@
 let canvas = getCanvas();
 let snowflake = getSnowflake(canvas);
 
+let iterations = getIterations();
+
 function getCanvas(){
     return document.getElementById('snowflake-generator');
 }
 
 function getSnowflake(canvas){
     return canvas.getContext('2d');
+}
+
+function getIterations(){
+    return document.getElementById("iterations").value;
 }
