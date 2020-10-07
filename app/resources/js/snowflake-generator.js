dbfd4e47cf049459b6cc735d5f3366c8e3221729
DimovDimo <d.dimov1996@gmail.com>
2020-10-07 08:41:24 +0300

    added function snowflakeTranslateSpin

--- app/resources/js/snowflake-generator.js
+++ app/resources/js/snowflake-generator.js
@@ -2,117 +2,120 @@ const centerLengthConstant = 2;
 const snowflakeLineConstant = "round";
 
 function changeEvent() {
-    let container = document.getElementById("container");
-    container.addEventListener("change", change);
+	let container = document.getElementById("container");
+	container.addEventListener("change", change);
 };
 
 changeEvent();
 
 function change() {
-    let canvas = getCanvas();
+	let canvas = getCanvas();
 	setCanvasSize(canvas);
-	
-    let snowflake = getSnowflake(canvas);
+
+	let snowflake = getSnowflake(canvas);
 	snowflakeStyle(canvas, snowflake);
-	
-    let iterations = getIterations();
+
+	let iterations = getIterations();
 	let splits = getSplits();
-	let angle = getAngle();	
+	let angle = getAngle();
 	let rays = getRays();
 	let offset = getOffset();
 	let spin = getSpin();
 
-	function raysIterations(snowflake, rays){
+	function raysIterations(snowflake, rays) {
 		for (let i = 1; i < rays; i++) {
 			paintRay(0);
 			snowflakeRotate(snowflake, rays);
 		}
 	}
-	
+
 	raysIterations(snowflake, rays);
 
 
-    function paintRay(iteration) {
-    	if(isClosePaintRay(iteration, iterations)) {
-    		return;
-    	}
-		
+	function paintRay(iteration) {
+		if (isClosePaintRay(iteration, iterations)) {
+			return;
+		}
+
 		snowflakeDraw(snowflake);
-   
-    function paintRecursion(){
-	    for (let startSplits = getStartSplits(); startSplits < splits; startSplits++) {
-            snowflake.save();
-			
-			let snowflakeTranslate = getSnowflakeTranslate(startSplits, splits, offset);
-			snowflake.translate(snowflakeTranslate, spin);
-      
-			let scaleWidthPercent = document.getElementById("scale-width").value;
-			let scaleHeightPercent = document.getElementById("scale-height").value;
-			
-			let scaleWidth = getScale(scaleWidthPercent);
-			let scaleHeight = getScale(scaleHeightPercent);
-			
-			snowflake.scale(scaleWidth, scaleHeight);
-			
-    		snowflake.save();    		
-    		snowflake.rotate(angle);
-    		paintRay(iteration + 1);    		
-    		snowflake.restore();
-    		
-    		snowflake.save();    		
-    		snowflake.rotate(-angle);
-    		paintRay(iteration + 1);    		
-    		snowflake.restore();
-    		
-    		snowflake.restore();
-        }
-    }
-	
-    paintRecursion();
-}
 
-paintRay(0);
+		function paintRecursion() {
+			for (let startSplits = getStartSplits(); startSplits < splits; startSplits++) {
+				snowflake.save();
 
+				snowflakeTranslateSpin(startSplits, splits, offset, snowflake, spin);
+
+				let scaleWidthPercent = document.getElementById("scale-width").value;
+				let scaleHeightPercent = document.getElementById("scale-height").value;
+
+				let scaleWidth = getScale(scaleWidthPercent);
+				let scaleHeight = getScale(scaleHeightPercent);
+
+				snowflake.scale(scaleWidth, scaleHeight);
+
+				snowflake.save();
+				snowflake.rotate(angle);
+				paintRay(iteration + 1);
+				snowflake.restore();
+
+				snowflake.save();
+				snowflake.rotate(-angle);
+				paintRay(iteration + 1);
+				snowflake.restore();
+
+				snowflake.restore();
+			}
+		}
+
+		paintRecursion();
+	}
+
+	paintRay(0);
 }
 
 change();
 
-function getCanvas(){
-    return document.getElementById("snowflake-generator");
+function snowflakeTranslateSpin(startSplits, splits, offset, snowflake, spin) {
+	let snowflakeTranslate = getSnowflakeTranslate(startSplits, splits, offset);
+	snowflake.translate(snowflakeTranslate, spin);
 }
 
-function getSnowflake(canvas){
-    return canvas.getContext("2d");
+function getCanvas() {
+	return document.getElementById("snowflake-generator");
 }
 
-function setCanvasSize(canvas){	
+function getSnowflake(canvas) {
+	return canvas.getContext("2d");
+}
+
+function setCanvasSize(canvas) {
 	canvas.width = document.getElementById("width").value;
-    canvas.height = document.getElementById("height").value;
+	canvas.height = document.getElementById("height").value;
 }
 
-function centerSnowflake(canvas, snowflake){
+function centerSnowflake(canvas, snowflake) {
 	let width = centerLength(canvas.width);
 	let height = centerLength(canvas.height);
-	
+
 	snowflake.translate(width, height);
 }
 
-function centerLength(length){
+function centerLength(length) {
 	return length / centerLengthConstant;
 }
 
-function getIterations(){
-    return document.getElementById("iterations").value;
+function getIterations() {
+	return document.getElementById("iterations").value;
 }
 
-function getSplits(){
+function getSplits() {
 	let splits = document.getElementById("splits").value;
-	
-    return ++splits;
+
+	return ++splits;
 }
 
-function getAngle(){
-    return document.getElementById("angle").value;
+function getAngle() {
+	return document.getElementById("angle").value;
 }
 
 function snowflakeStyle(canvas, snowflake) {
@@ -124,98 +127,98 @@ function snowflakeStyle(canvas, snowflake) {
 	setLineCap(snowflake);
 }
 
-function getBackgroundColor(){
-    return document.getElementById("background").value;
+function getBackgroundColor() {
+	return document.getElementById("background").value;
 }
 
-function setBackgroundColor(canvas, snowflake){
+function setBackgroundColor(canvas, snowflake) {
 	snowflake.fillStyle = getBackgroundColor();
-	
+
 	let xCoordinate = -canvas.width;
 	let yCoordinate = -canvas.height;
 	let width = canvas.width * centerLengthConstant;
-	let height = canvas.height * centerLengthConstant;	
+	let height = canvas.height * centerLengthConstant;
 	snowflake.fillRect(xCoordinate, yCoordinate, width, height);
 }
 
-function getRays(){
-    return document.getElementById("rays-count").value;
+function getRays() {
+	return document.getElementById("rays-count").value;
 }
 
-function snowflakeRotate(snowflake, rays){
-    snowflake.rotate((centerLengthConstant * Math.PI) / rays);;
+function snowflakeRotate(snowflake, rays) {
+	snowflake.rotate((centerLengthConstant * Math.PI) / rays);;
 }
 
-function setSnowflakeAngle(snowflake){
+function setSnowflakeAngle(snowflake) {
 	let snowflakeAngle = document.getElementById("snowflake-angle").value;
 	let degrees = -snowflakeAngle;
-	
+
 	snowflake.rotate(degreesToRadians(degrees));
 }
 
-function degreesToRadians(degrees){
+function degreesToRadians(degrees) {
 	return (degrees * Math.PI) / 180;
 }
 
-function isClosePaintRay(iteration, iterations){
+function isClosePaintRay(iteration, iterations) {
 	return iteration > iterations;
 }
 
-function setSnowflakeColor(snowflake){
+function setSnowflakeColor(snowflake) {
 	let snowflakeColor = document.getElementById("snowflake-color").value;
-    snowflake.strokeStyle = snowflakeColor;
+	snowflake.strokeStyle = snowflakeColor;
 }
 
-function setLineWidth(snowflake){
+function setLineWidth(snowflake) {
 	let lineWidth = document.getElementById("line-width").value;
-    snowflake.lineWidth = lineWidth;
+	snowflake.lineWidth = lineWidth;
 }
 
-function setLineCap(snowflake){
+function setLineCap(snowflake) {
 	snowflake.lineCap = snowflakeLineConstant;
 }
 
-function snowflakeRays(snowflake){
+function snowflakeRays(snowflake) {
 	snowflakeMove(snowflake);
 	snowflakeLine(snowflake);
 }
 
-function snowflakeMove(snowflake){
+function snowflakeMove(snowflake) {
 	let raysBasis = document.getElementById("rays-basis").value;
 	let raysRotation = document.getElementById("rays-rotation").value;
-	
+
 	snowflake.moveTo(raysBasis, raysRotation);
 }
 
-function snowflakeLine(snowflake){
+function snowflakeLine(snowflake) {
 	let raysLength = document.getElementById("rays-length").value;
 	let raysDisplacement = document.getElementById("rays-displacement").value;
-	
+
 	snowflake.lineTo(raysLength, raysDisplacement);
 }
 
-function snowflakeDraw(snowflake){
+function snowflakeDraw(snowflake) {
 	snowflake.beginPath();
 	snowflakeRays(snowflake);
 	snowflake.stroke();
 }
 
-function getStartSplits(){
-    return document.getElementById("start-splits").value;
+function getStartSplits() {
+	return document.getElementById("start-splits").value;
 }
 
-function getOffset(){
-    return document.getElementById("rays-offset").value;
+function getOffset() {
+	return document.getElementById("rays-offset").value;
 }
 
-function getSpin(){
-    return document.getElementById("rays-spin").value;
+function getSpin() {
+	return document.getElementById("rays-spin").value;
 }
 
-function getSnowflakeTranslate(startSplits, splits, offset){
+function getSnowflakeTranslate(startSplits, splits, offset) {
 	return (offset * startSplits) / splits;
 }
 
-function getScale(percent){
+function getScale(percent) {
 	return percent / 100;
 }
