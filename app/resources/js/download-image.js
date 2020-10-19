1465790b4181ad4e8491e512cb3500ef5eb4b1b1
DimovDimo <d.dimov1996@gmail.com>
2020-10-19 10:43:32 +0300

    added function download

--- app/resources/js/download-image.js
+++ app/resources/js/download-image.js
@@ -1,5 +1,11 @@
 document.getElementById("download-image").addEventListener("click", downloadImage);
 
 function downloadImage() {
-	
+	//window.stop();
+}
+
+function download() {
+    let canvasElement = document.getElementById("snowflake-generator");
+    let type = "image/png";
+    let url = canvasElement.toDataURL(type);
 }
