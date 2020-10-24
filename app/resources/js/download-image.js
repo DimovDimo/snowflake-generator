d184a2e0d62cfb2fa8d65fb872d410bd887c9adf
DimovDimo <d.dimov1996@gmail.com>
2020-10-24 07:39:57 +0300

    added imageTypeConstant

--- app/resources/js/download-image.js
+++ app/resources/js/download-image.js
@@ -1,27 +1,19 @@
+const imageTypeConstant = "image/png";
+
 document.getElementById("download-image").addEventListener("click", download);
 
 function download() {
     let link = document.createElement("a");
     link.download = document.getElementById("file-name").value;
 
-    let type = document.querySelector('input[name="image-type"]:checked').value;
     let canvas = document.getElementById("snowflake-generator");
-    let quality = getQuality(type);
-    let image = canvas.toDataURL(type, quality);
+    let image = canvas.toDataURL(imageTypeConstant);
     link.href = image;
 
-    let datasetDownload = [type, link.download, link.href];
+    let datasetDownload = [imageTypeConstant, link.download, link.href];
     link.dataset.downloadurl = datasetDownload.join(":");
 
     document.body.appendChild(link);
 	link.click();
 	document.body.removeChild(link);
-}
-
-function getQuality(type) {
-	if(type.localeCompare("image/jpeg")){
-        return document.getElementById("snowflake-generator").value / 100;
-    }
-
-    return undefined;
 }
